7cc7cc06326516ce9108e0bb3819dd5d1d1e6457
AnastasiaTrefoil <ana.tref.1075@gmail.com>
2017-11-30 11:19:04 +0300

    + Proxy task

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  */
 function forEach(array, fn) {
 
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         fn(array[i], i, array);
     }
 
@@ -18,9 +18,9 @@ function forEach(array, fn) {
  */
 function map(array, fn) {
 
-    var arr = [];
+    let arr = [];
 
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         arr.push(fn(array[i], i, array));
     }
 
@@ -32,15 +32,13 @@ function map(array, fn) {
  Напишите аналог встроенного метода reduce для работы с массивами
  */
 function reduce(array, fn, initial) {
-  
-    var result = array[0],
+
+    let result = array[0],
         i = 1;
 
     if (initial) {
-
         result = initial;
         i = 0;
-
     }
 
     for (; i < array.length; i++) {
@@ -50,6 +48,7 @@ function reduce(array, fn, initial) {
     return result;
 
 }
+
 /*
  Задача 4:
  Функция принимает объект и имя свойства, которое необходиом удалить из объекта
@@ -77,10 +76,10 @@ function hasProperty(obj, prop) {
  */
 function getEnumProps(obj) {
 
-    var propArr = [];
+    let propArr = [];
 
-    for ( var prop in obj) {
-        propArr.push(prop);
+    for ( let prop in obj) {
+    	propArr.push(prop);
     }
 
     return propArr;
@@ -92,16 +91,15 @@ function getEnumProps(obj) {
  */
 function upperProps(obj) {
 
-    var propArr = [];
+	let propArr = [];
 
-    for ( var prop in obj) {
-
-        propArr.push(prop.toUpperCase());
-
+    for ( let prop in obj) {
+    	propArr.push(prop.toUpperCase());
     }
 
     return propArr;
-    
+
+
 }
 
 /*
@@ -111,27 +109,39 @@ function upperProps(obj) {
 
 function slice(array, from, to) {
 
-    var newArr = [];
+    let newArr = [];
 
     from = from === undefined ? 0 : (typeof from !== 'number' ? 0 : (from < 0 ? (array.length + from < 0 ? 0 : array.length + from) : from));
-    
-    to = to === undefined ? array.length : (typeof to === 'number' ? (to < 0 ? array.length + to : (to > array.length ? array.length : to)) : '');
-    
-    if (to !== '') {
 
+    to = to === undefined ? array.length : (typeof to  === 'number' ? (to < 0 ? array.length + to : (to > array.length ? array.length : to)) : '');
+
+    if (to !== '') {
+    
         for (; from < to; from++) {
             newArr.push(array[from]);
         }
     }
 
-    return newArr;
+	return newArr;
+
 }
+
 /*
  Задача 9 *:
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+
+    let proxy = new Proxy(obj, {
+        set (target, prop, value) {
+            target[prop] = value * value;
+            return true;
+        }
+    });    
+
+    return proxy;
+}
 
 export {
     forEach,
